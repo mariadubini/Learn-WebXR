682ee9107a6740e3c2c4ca21ea0b817b30493aa1
mariadubini <74077744+mariadubini@users.noreply.github.com>
2021-07-01 18:05:06 -0300

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -5,6 +5,29 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
+		this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+		this.camera.position.set (0,0,4);
+		
+		this.scene =new THREE.Scene();
+		this.scene.background = new THREE.Color (0xaaaaa);
+		
+		const ambient =new THREE.HemisphereLight (0xffffff, 0xbbbff, 0.3);
+		this.scene.add(ambient);
+		
+		const light =new THREE.DirectionalLight();
+		light.position.set (0.2, 1, 1);
+		this.scene.add(light);
+		
+		this.renderer =new THREE.WebGLRenderer ({antialias:true});
+		this.renderer.setPixelRatio (window.innerWidth, window.innerHeight);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
+		
+		this.renderer.setAnimationLoop(this.render.bind(this));
+		
+		const geometry = new THREE.BoxBufferGeometry();
+		const material= new THREE.MeshStandardMaterial({ color:0xff0000)};
+		this.mesh = new THREE.Mesh (geometry, material);
     
         
         window.addEventListener('resize', this.resize.bind(this) );
